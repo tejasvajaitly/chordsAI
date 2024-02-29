036a7fd61473ccb215a0f7b67bd0683638036c39
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-02-28 19:39:51 -0800

    implement spotify login feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Scopes } from "@spotify/web-api-ts-sdk";
 import "./App.css";
+import { useSpotify } from "./hooks/useSpotify";
 
 function App() {
-    return <div>chordsAI</div>;
+    const sdk = useSpotify(
+        import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+        import.meta.env.VITE_REDIRECT_TARGET,
+        Scopes.all
+    );
+    return sdk ? (
+        <div>you are logged in</div>
+    ) : (
+        <>you should be redirected soon</>
+    );
 }
 
 export default App;
