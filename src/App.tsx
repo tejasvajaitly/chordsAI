23c4139fbc9be83464151d19ecda8773b928749c
Tejasva Jaitly <tejasvajaitly@gmail.com>
2024-02-29 22:56:08 -0800

    implement get users playlists component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import { Scopes } from "@spotify/web-api-ts-sdk";
+import {
+    Scopes,
+    SimplifiedPlaylist,
+    SpotifyApi,
+} from "@spotify/web-api-ts-sdk";
 import "./App.css";
 import { useSpotify } from "./hooks/useSpotify";
+import { useEffect, useState } from "react";
 
 function App() {
     const sdk = useSpotify(
@@ -9,10 +14,40 @@ function App() {
         Scopes.all
     );
     return sdk ? (
-        <div>you are logged in</div>
+        <UserPlaylists sdk={sdk} />
     ) : (
         <>you should be redirected soon</>
     );
 }
 
 export default App;
+
+type UserPlaylistsProps = {
+    sdk: SpotifyApi;
+};
+
+function UserPlaylists(props: UserPlaylistsProps) {
+    const { sdk } = props;
+    const [userPlaylists, setUserPlaylists] = useState<
+        SimplifiedPlaylist[] | null
+    >(null);
+
+    useEffect(() => {
+        const getUP = async () => {
+            try {
+                const results = await sdk.currentUser.playlists.playlists(50);
+                if (results.items) setUserPlaylists(results.items);
+            } catch (e: Error | unknown) {
+                console.log(e);
+            }
+        };
+
+        getUP();
+    }, []);
+
+    return userPlaylists ? (
+        userPlaylists.map((userPlaylist) => <div>{userPlaylist.name}</div>)
+    ) : (
+        <>either loading or error getting data</>
+    );
+}
